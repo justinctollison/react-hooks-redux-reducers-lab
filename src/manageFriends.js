f22761034d3d3e8bcd6c904a08ce8915262a158b
justinctollison <justinctollison@gmail.com>
2022-03-19 03:17:32 -0700

    Completed assignment

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,15 @@
 export function manageFriends(state, action) {
   // your code here
+  switch(action.type) {
+    case"friends/add":
+      return {
+        friends: [...state.friends, action.payload]
+      }
+    case"friends/remove":
+      return {
+        friends: state.friends.filter((friends) => friends.id !== action.payload)
+      }
+    default: 
+      return state
+  }
 }
